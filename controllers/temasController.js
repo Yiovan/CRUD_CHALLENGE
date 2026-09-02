944bcd6a40f8f50217659fb7780d909f0769d89e
Yiovan <giovannicabrerarivas@icloud.com>
2026-09-02 17:00:22 -0300

    feat: modificacion del temasControler

--- controllers/temasController.js
+++ controllers/temasController.js
@@ -1,9 +1,24 @@
 const temas = require('../models/temas');
 
 const obtenerTemas = (req, res) => {
-    res.json(temas);
+    res.render('index', { temas: temas });
 };
 
+// NUEVA FUNCIÓN: Recibe el formulario y crea el tema
+const crearTema = (req, res) => {
+    const nuevoTema = {
+        id: Date.now(), // Crea un número único rápido para el ID
+        titulo: req.body.titulo, // Atrapa lo que el usuario escribió en el "input"
+        votos: 0,
+        enlaces: []
+    };
+    
+    temas.push(nuevoTema); // Lo guardamos en nuestra "base de datos"
+    res.redirect('/'); // Recargamos la página para ver el cambio
+};
+
+// Exportamos ambas funciones
 module.exports = {
-    obtenerTemas
-}
+    obtenerTemas,
+    crearTema
+};
