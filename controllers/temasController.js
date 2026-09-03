dd36e6a220a3c3cfbb8ad656f5f3f73c5045ad08
Yiovan <giovannicabrerarivas@icloud.com>
2026-09-03 13:53:41 -0300

    fix: arreglo del archivo temasController

--- controllers/temasController.js
+++ controllers/temasController.js
@@ -1,19 +1,21 @@
 const temas = require('../models/temas');
 
-const obtenerTemas = (req, res) => {
-    res.render('index', { temas: temas });
+const obtenerTemas = async (req, res) => {
+    const listaTemas = await temas.obtenerTodos();
+    res.render('index', { temas: listaTemas });
 };
 
 // NUEVA FUNCIÓN: Recibe el formulario y crea el tema
-const crearTema = (req, res) => {
-    const nuevoTema = {
-        id: Date.now(), // Crea un número único rápido para el ID
-        titulo: req.body.titulo, // Atrapa lo que el usuario escribió en el "input"
-        votos: 0,
-        enlaces: []
-    };
+const crearTema = async (req, res) => {
+    // const nuevoTema = {
+    //     id: Date.now(), // Crea un número único rápido para el ID
+    //     titulo: req.body.titulo, // Atrapa lo que el usuario escribió en el "input"
+    //     votos: 0,
+    //     enlaces: []
+    // };
     
-    temas.push(nuevoTema); // Lo guardamos en nuestra "base de datos"
+    // temas.push(nuevoTema); // Lo guardamos en nuestra "base de datos"
+    await temas.crear(req.body.titulo);
     res.redirect('/'); // Recargamos la página para ver el cambio
 };
 
