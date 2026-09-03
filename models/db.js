5f3d475a427eab366267fcf645920e3f888b26c6
Yiovan <giovannicabrerarivas@icloud.com>
2026-09-03 11:07:08 -0300

    feat: ingreso de db

---
+++ models/db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg')
+
+const pool = new Pool ({
+    user: 'admin',
+    host: 'localhost',
+    database: 'learndb',
+    password: 'password123',
+    port: 5432,
+})
+
+
+const inicializarDB = async ()=> {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS temas (
+            id SERIAL PRIMARY KEY,
+            titulo VARCHAR(255) NOT NULL,
+            votos INTEGER DEFAULT 0
+        );
+    `);
+    console.log("base de datos lista y creada correctamente")
+}
